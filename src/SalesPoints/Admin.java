690d415883986c73ac9999dca486146e5ecc6db5
Jaiber Diaz <djaiver9@gmail.com>
2023-05-05 17:40:09 -0500

    Menu administrador y opciones

--- src/SalesPoints/Admin.java
+++ src/SalesPoints/Admin.java
@@ -1,8 +1,11 @@
 package SalesPoints;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 
-public class Admin extends User implements Serializable {
+public class Admin extends User implements Serializable{
 
 
     public Admin (String userID, String nameUser, String emailUser, String password) {
@@ -33,11 +36,31 @@ public class Admin extends User implements Serializable {
         return super.getPasswordUser();
     }
     @Override
-    public boolean verifyLogin(String adminNameAux, String adminPasswordAux) {
-        if (adminNameAux == getNameUser()&& adminPasswordAux== getPasswordUser()) {
-           System.out.println("HA INGRESADO AL SISTEMA");
+    public int login(String userNameAux, String passwordAuxAux) throws IOException {
+
+        System.out.println("Ingresó como ADMINISTRADOR");
+        System.out.println("------------------ MENÚ ADMINISTRADOR --------------------");
+        System.out.println("Digite 1 para CHECK SALES y 2 para  SALIR:");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int optionAdmin = Integer.parseInt(in.readLine());
+        while (optionAdmin != 1 && optionAdmin != 2) {
+            System.out.println("!Opción no válida");
+            System.out.println("Ingrese una nueva opción:");
+            optionAdmin = Integer.parseInt(in.readLine());
         }
-        return true;
+        return optionAdmin;
+    }
+    public int checkSales(int lookSales) throws IOException{
+        System.out.println("REVISANDO LAS VENTAS");
+        System.out.println("\tDigite 2 para SALIR:");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int optionAdmin = Integer.parseInt(in.readLine());
+        while (optionAdmin != 2) {
+            System.out.println("!Opción no válida");
+            System.out.println("Ingrese una nueva opción:");
+            optionAdmin = Integer.parseInt(in.readLine());
+        }
+        return optionAdmin;
     }
 
     @Override
