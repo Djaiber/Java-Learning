55dcf383a057a94a6e102ee66b89903db866b053
Jaiber Diaz <djaiver9@gmail.com>
2023-05-05 17:42:49 -0500

    Implemetación metodos de Admin

--- src/SalesPoints/Main.java
+++ src/SalesPoints/Main.java
@@ -21,17 +21,16 @@ public class Main {
         objectOutStreamAdmin.writeObject(admin);
         objectOutStreamAdmin.close();
         //Lee e imprime los atributos del administrador (admin)
-        FileInputStream fileInputStream = new FileInputStream("ObjetoAdmin.txt");//Método para leer los atributos del admin
+        FileInputStream fileInputStream = new FileInputStream("ObjetoAdmin.txt");
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         Admin savedAdmin = (Admin) objectInputStream.readObject();
-        System.out.println("------ADMINISTRADOR------");
         System.out.println(savedAdmin);
         objectInputStream.close();
 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("----------------------------------------------------------");
-        System.out.println("-------------------DISTRIPIZZA POINTS---------------------");
+        System.out.println("------------------ DISTRIPIZZA POINTS --------------------");
         System.out.println("------------------------------¡Donde tus compras cuenta!--");
         System.out.println("Digite 1 para LOGIN or 2 para SIGN IN:");
         System.out.println("1. LOGIN");
@@ -45,25 +44,38 @@ public class Main {
             }
             //Validación de opción
             while (menu != 1 && menu != 2) {
-                System.out.println("!Opción no válida");
+                System.out.println("!Opción no válida¡");
                 System.out.println("Ingrese una nueva opción:");
                 menu = Integer.parseInt(in.readLine());
             }
             if (menu == 1) {
                 System.out.println("----------------------------------------------------------");
-                System.out.println("--------------------------LOGIN---------------------------");
+                System.out.println("------------------------- LOGIN --------------------------");
                 System.out.println("----------------------------------------------------------");
                 System.out.println("\t Ingrese su nombre de usuario: ");
                 String userNameAux = in.readLine();
                 System.out.println("\t Ingrese su password:");
                 String passwordAux = in.readLine();
                 if (userNameAux.equals(admin.getNameUser()) && passwordAux.equals(admin.getPasswordUser())) {
-                    System.out.println("Ingresó como ADMINISTRADOR");
+                    int login = admin.login(userNameAux,passwordAux);
+                    int checkSales = 0;
+                    if (login==1){
+                        checkSales = admin.checkSales(login);
+                    }
+                    if (login==2|| checkSales==2){
+                        System.out.println("\t Ha salido del MENÚ ADMINISTRADOR");
+                    }
+
                 }
+                if (!userNameAux.equals(admin.getNameUser()) && !passwordAux.equals(admin.getPasswordUser())) {
+                    int login = cliente1.login(userNameAux,passwordAux);
+                }
+
+
             }
             if (menu == 2) {
                 System.out.println("----------------------------------------------------------");
-                System.out.println("-------------------------SIGN IN--------------------------");
+                System.out.println("------------------------ SIGN IN -------------------------");
                 System.out.println("----------------------------------------------------------");
 
                 int randomUserID = (int) (Math.random() * 999999);
@@ -111,7 +123,7 @@ public class Main {
                 System.out.println("Nuevos datos = " + clienteNew);
             }
             System.out.println("----------------------------------------------------------");
-            System.out.println("-------------------DISTRIPIZZA POINTS---------------------");
+            System.out.println("------------------ DISTRIPIZZA POINTS --------------------");
             System.out.println("------------------------------¡Donde tus compras cuenta!--");
             System.out.println("Digite 1 para LOGIN o 2 para SIGN IN:");
             System.out.println("1.LOGIN");
@@ -123,7 +135,7 @@ public class Main {
             }
             //Validación de opción
             while (menu != 1 && menu != 2) {
-                System.out.println("!Opción no válida");
+                System.out.println("!Opción no válida¡");
                 System.out.println("Ingrese una nueva opción:");
                 menu = Integer.parseInt(in.readLine());
             }
