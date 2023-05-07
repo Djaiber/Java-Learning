624f7996a648805c76dc264ce522d0e97b2fc042
Jaiber Diaz <djaiver9@gmail.com>
2023-05-07 00:42:12 -0500

    método registrar y opciones menu cliente

--- src/SalesPoints/Cliente.java
+++ src/SalesPoints/Cliente.java
@@ -1,9 +1,8 @@
 package SalesPoints;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Serializable;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Cliente extends User implements Serializable {
 
@@ -78,27 +77,66 @@ public class Cliente extends User implements Serializable {
         this.bonoPoints = bonoPoints;
 }
 
-    public String registerUser(){
-        System.out.println("REGISTRAR NUEVO CLIENTE");
+    public String getResgisterDate() { return resgisterDate;}
+    public void setResgisterDate(String resgisterDate) {this.resgisterDate = resgisterDate;}
+
+    //Método para registrar un nuevo cliente
+    public static Cliente registerCliente(int goodOption) throws IOException, ClassNotFoundException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int randomUserID = (int) (Math.random() * 999999);
+        String ClienteIDAux = Integer.toString(randomUserID);
         System.out.println("Ingrese su nombre:");
-        return null;
+        String ClienteUserNameAux = in.readLine();
+        System.out.println("Ingrese su correo:");
+        String ClienteEmailUserAux = in.readLine();
+        System.out.println("Ingrese su Contraseña:");
+        String ClientePasswordUserAux = in.readLine();
+        System.out.println("Ingrese su teléfono:");
+        String ClientePhoneNumberAux = in.readLine();
+
+        //Método para crear el atributo de registerDate del Cliente
+        LocalDate actualDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        String ClienteRegisterDateAux = actualDate.format(formatter);
+
+        //Asigna los datos ingresados por teclado como atributos del objeto clienteNew
+        Cliente clienteNew = new Cliente(ClienteIDAux, ClienteUserNameAux,
+                ClienteEmailUserAux, ClientePhoneNumberAux,
+                ClientePasswordUserAux, ClienteRegisterDateAux, 0);
+
+        //Escribe los atributos de Cliente registrado en el fichero
+        FileOutputStream fileOutputStreamCliente = new FileOutputStream("ObjetoCliente.txt");
+        ObjectOutputStream objectOutStreamCliente = new ObjectOutputStream(fileOutputStreamCliente);
+        objectOutStreamCliente.writeObject(clienteNew);
+        objectOutStreamCliente.close();
+        //Lee e imprime los atributos del cliente registrado (clienteNew)
+        FileInputStream fileInputStreamCliente = new FileInputStream("ObjetoCliente.txt");//Método para leer los atributos del admin
+        ObjectInputStream objectInputStreamCliente = new ObjectInputStream(fileInputStreamCliente);
+        Cliente savedcliente = (Cliente) objectInputStreamCliente.readObject();
+        System.out.println("------CLIENTE REGISTRADO------");
+        System.out.println(savedcliente);
+        objectInputStreamCliente.close();
+
+        return clienteNew;
     }
-    public String getResgisterDate() {
-        return resgisterDate;
-    }
-    public void setResgisterDate(String resgisterDate) {
-        this.resgisterDate = resgisterDate;
-    }
+
+    //Método para ingresar como cliente
     @Override
     public int login(String userID, String password) throws IOException {
         System.out.println("Ingresó como CLIENTE");
         System.out.println("------------------ MENÚ CLIENTE --------------------");
-        System.out.println("Digite 1 para CHECK SALES y 2 para  SALIR:");
+        System.out.println("Digite 1 para REGISTER BUY; 2 para SHOW HISTORIAL; 3 para UPDATE PROFILE y 4 para SALIR:");
+        System.out.println("1. REGISTER BUY");
+        System.out.println("2. SHOW HISTORIAL");
+        System.out.println("3. UPDATE PROFILE");
+        System.out.println("4. SALIR");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int optionCliente = Integer.parseInt(in.readLine());
-        return optionCliente;
+        int menu =0;
+        menu = CommonMethods.ExceptionMenuCliente(menu);
+        return menu;
     }
-
+    //Formato CSV
     @Override
     public String toString() {
         return userID +
