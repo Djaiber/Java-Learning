0968636d805bac8439ff6f06d1974c3cba94040a
Jaiber Diaz <djaiver9@gmail.com>
2023-05-07 00:43:04 -0500

    Métodos para excepciones en menu admi y cliente

--- src/SalesPoints/CommonMethods.java
+++ src/SalesPoints/CommonMethods.java
@@ -6,16 +6,54 @@ import java.io.InputStreamReader;
 
 public class CommonMethods {
 
-    public static int ExceptionGod(int menu) throws IOException {
+
+
+    public static int ExceptionMenuMain(int menu) throws IOException {
 
         while (true) {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 menu = Integer.parseInt(in.readLine());
+                //Opción (25) para apagar el programa
                 if (menu == 1 || menu == 2 || menu == 25) {
                     if (menu == 25) {
                         System.exit(0);
                     }
+                    break;
+                }
+                System.out.println("Digite un número válido");
+            } catch (Exception e) {
+                System.out.println("Digite una opción valida");
+            }
+        }
+        return menu;
+    }
+
+    public static int ExceptionMenuAdmin(int menu) throws IOException {
+
+        while (true) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                menu = Integer.parseInt(in.readLine());
+                //Opción (25) para apagar el programa
+                if (menu == 2) {
+                    break;
+                }
+                System.out.println("Digite un número válido");
+            } catch (Exception e) {
+                System.out.println("Digite una opción valida");
+            }
+        }
+        return menu;
+    }
+    public static int ExceptionMenuCliente(int menu) throws IOException {
+
+        while (true) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                menu = Integer.parseInt(in.readLine());
+                //Opción (25) para apagar el programa
+                if (menu== 1 || menu == 2 || menu == 3 || menu == 4) {
                     break;
                 }
                 System.out.println("Digite un número válido");
