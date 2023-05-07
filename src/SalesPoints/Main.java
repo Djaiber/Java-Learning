de4c1d1a8f953d6ea533e78c5a27c2bcb8006d03
Jaiber Diaz <djaiver9@gmail.com>
2023-05-07 00:44:23 -0500

    Traslado método registarCliente a Class Cliente

--- src/SalesPoints/Main.java
+++ src/SalesPoints/Main.java
@@ -1,9 +1,6 @@
 package SalesPoints;
 
 import java.io.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Random;
 
 
 public class Main {
@@ -37,7 +34,7 @@ public class Main {
         System.out.println("2. SIGN IN");
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int menu =0;
-        menu = CommonMethods.ExceptionGod(menu);
+        menu = CommonMethods.ExceptionMenuMain(menu);
         int goodOption = menu;
         do {
 
@@ -58,61 +55,32 @@ public class Main {
                     if (login==2|| checkSales==2){
                         System.out.println("\t Ha salido del MENÚ ADMINISTRADOR");
                     }
-
                 }
                 if (!userNameAux.equals(admin.getNameUser()) && !passwordAux.equals(admin.getPasswordUser())) {
                     int login = cliente1.login(userNameAux,passwordAux);
                 }
-
+                //DEFINIR MÉTODOS
+                //1. register buy
+                //2. show historial
+                //3. update profile
+                //4. Salir menu cliente
 
             }
             if (goodOption == 2) {
                 System.out.println("----------------------------------------------------------");
                 System.out.println("------------------------ SIGN IN -------------------------");
                 System.out.println("----------------------------------------------------------");
+                //Método para registrar un nuevo cliente
+                Cliente registered = Cliente.registerCliente(goodOption);
 
-                int randomUserID = (int) (Math.random() * 999999);
-                String ClienteIDAux = Integer.toString(randomUserID);
-                System.out.println("Ingrese su nombre:");
-                String ClienteUserNameAux = in.readLine();
-                System.out.println("Ingrese su correo:");
-                String ClienteEmailUserAux = in.readLine();
-                System.out.println("Ingrese su teléfono:");
-                String ClientePhoneNumberAux = in.readLine();
-                System.out.println("Ingrese su Contraseña:");
-                String ClientePasswordUserAux = in.readLine();
-
-                //Método para crear el atributo de registerDate del Cliente
-                LocalDate actualDate = LocalDate.now();
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-                String ClienteRegisterDateAux = actualDate.format(formatter);
-
-                //Asigna los datos ingresados por teclado como atributos del objeto clienteNew
-                Cliente clienteNew = new Cliente(ClienteIDAux, ClienteUserNameAux,
-                        ClienteEmailUserAux, ClientePhoneNumberAux,
-                        ClientePasswordUserAux, ClienteRegisterDateAux, 0);
-
-                //Escribe los atributos de Cliente registrado en el fichero
-                FileOutputStream fileOutputStreamCliente = new FileOutputStream("ObjetoCliente.txt");
-                ObjectOutputStream objectOutStreamCliente = new ObjectOutputStream(fileOutputStreamCliente);
-                objectOutStreamCliente.writeObject(clienteNew);
-                objectOutStreamCliente.close();
-
-                //Lee e imprime los atributos del cliente registrado (clienteNew)
-                FileInputStream fileInputStreamCliente = new FileInputStream("ObjetoCliente.txt");//Método para leer los atributos del admin
-                ObjectInputStream objectInputStreamCliente = new ObjectInputStream(fileInputStreamCliente);
-                Cliente savedcliente = (Cliente) objectInputStreamCliente.readObject();
-                System.out.println("------CLIENTE------");
-                System.out.println(savedcliente);
-                objectInputStreamCliente.close();
-
-                //Limpia los atributos del objeto clienteNew
-                clienteNew.setUserID(null);
-                clienteNew.setNameUser(null);
-                clienteNew.setEmailUser(null);
-                clienteNew.setPhoneNumber(null);
-                clienteNew.setResgisterDate(null);
-                clienteNew.setPasswordUser(null);
+                //Limpia los atributos del objeto registered
+                registered.setUserID(null);
+                registered.setNameUser(null);
+                registered.setEmailUser(null);
+                registered.setPhoneNumber(null);
+                registered.setResgisterDate(null);
+                registered.setPasswordUser(null);
+                System.out.println("Cliente Borrado:"+ registered);
             }
             System.out.println("----------------------------------------------------------");
             System.out.println("------------------ DISTRIPIZZA POINTS --------------------");
@@ -120,17 +88,8 @@ public class Main {
             System.out.println("Digite 1 para LOGIN o 2 para SIGN IN:");
             System.out.println("1.LOGIN");
             System.out.println("2.SIGN IN");
-            goodOption = Integer.parseInt(in.readLine());
-            //Opción para apagar el programa
-            if (goodOption == 25) {
-                break;
-            }
-            //Validación de opción
-            while (goodOption != 1 && goodOption != 2) {
-                System.out.println("!Opción no válida¡");
-                System.out.println("Ingrese una nueva opción:");
-                goodOption = Integer.parseInt(in.readLine());
-            }
+            menu = CommonMethods.ExceptionMenuMain(menu);
+            goodOption = menu;
         }while (goodOption != 25) ;
     }
 }
