7cfbb2d6aa52944755135d61c891bc1e5a1b33aa
Jaiber Diaz <djaiver9@gmail.com>
2023-05-09 13:15:25 -0500

    Excepción MenuRecompena y MenuBono

--- src/SalesPoints/CommonMethods.java
+++ src/SalesPoints/CommonMethods.java
@@ -5,12 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CommonMethods {
-    public int ContadorClientes;
 
-    public CommonMethods(int contadorClientes) {
-        ContadorClientes = contadorClientes;
-    }
-
+    //Excepción para validar opciones (1,2,25)
     public static int ExceptionMenuMain(int menu) throws IOException {
 
         while (true) {
@@ -32,13 +28,13 @@ public class CommonMethods {
         return menu;
     }
 
+    //Excepción para validar opciones (1,2,3)
     public static int ExceptionMenuAdmin(int menu) throws IOException {
 
         while (true) {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 menu = Integer.parseInt(in.readLine());
-                //Opción (25) para apagar el programa
                 if (menu==1||menu == 2||menu==3) {
                     break;
                 }
@@ -49,14 +45,49 @@ public class CommonMethods {
         }
         return menu;
     }
+
+    //Excepción para validar opciones (1,2,3,4,5)
     public static int ExceptionMenuCliente(int menu) throws IOException {
 
         while (true) {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                 menu = Integer.parseInt(in.readLine());
-                //Opción (25) para apagar el programa
                 if (menu== 1 || menu == 2 || menu == 3 || menu == 4 || menu==5) {
+                    break;
+                }
+                System.out.println("Digite un número válido");
+            } catch (Exception e) {
+                System.out.println("Digite una opción valida");
+            }
+        }
+        return menu;
+    }
+    //Excepción para validar opciones (1,2,3,4)
+    public static int ExceptionMenuRecompensa(int menu) throws IOException {
+
+        while (true) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                menu = Integer.parseInt(in.readLine());
+                if (menu == 1 || menu == 2 || menu == 3 || menu == 4) {
+                    break;
+                }
+                System.out.println("Digite un número válido");
+            } catch (Exception e) {
+                System.out.println("Digite una opción valida");
+            }
+        }
+        return menu;
+    }
+    //Excepción para validar opciones (1,2)
+    public static int ExceptionMenuBono(int menu) throws IOException {
+
+        while (true) {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+                menu = Integer.parseInt(in.readLine());
+                if (menu == 1 || menu == 2) {
                     break;
                 }
                 System.out.println("Digite un número válido");
